e84b6d369e6d4fd632f36e5a36df5718530f0db7
Michael <vasquezloperamichael@gmail.com>
2022-10-19 10:47:32 -0500

    2da entrega

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { ItemCount } from "./ItemCount/ItemCount.jsx";
 import { ItemListContainer } from "./itemListContainer/ItemListContainer.js";
-import { NavBar } from "./navBar/NavBar.js";
+// import { NavBar } from "./navBar/NavBar.js";
+
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar /> */}
       <ItemListContainer  greeting={'pasando un texto por props'}/>
+      <ItemCount />
     </div>
   );
 }
